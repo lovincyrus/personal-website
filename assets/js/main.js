fa3a852514ee9cd6d6306d617c5b90a9a2ce19ba
lovincyrus <hello@lovincyrus.com>
2017-03-10 22:28:28 -0800

    Add animated sayHello

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/**
+
+1. document ready
+2. typed.js > fadeOut
+3. img and navbar > fadeIn
+
+**/
+
+document.addEventListener("DOMContentLoaded", myFunction);
+
+function myFunction() {
+	Typed.new(".sayHello", {
+	strings: ["Hello ✨ ^1000", "I'm glad that you're here. ^1000", "No! ^10", "No! ^10", "No! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful, thankful. 🙏 🙏🏻 🙏🏼 🙏🏽 🙏🏾 🙏🏿 ^1000"]
+	// loop: true
+	// typeSpeed: 50
+	});
+
+	$("img").hide();
+	$("ul.menu").hide();
+	$("img").delay(13000).show(1000);
+	$("ul.menu").delay(13000).show(1000);
+	$(".sayHello").delay(16000).fadeOut();
+}
