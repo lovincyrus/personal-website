4ea308da8b0e2808d16ed7462547886f4114fef2
lovincyrus <hello@lovincyrus.com>
2017-03-14 20:15:08 -0700

    Remove emojis && increase font-size of sayHello

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,16 @@ document.addEventListener("DOMContentLoaded", myFunction);
 
 function myFunction() {
 	Typed.new(".sayHello", {
-	strings: ["Hello ✨ ^1000", "I'm glad that you're here. ^1000", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful, thankful. 🙏 🙏🏻 🙏🏼 🙏🏽 🙏🏾 🙏🏿 ^1000"],
+	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful, thankful that you're here. ^100"]
 	// loop: true
-	typeSpeed: 30
+	// typeSpeed: 30
 	});
 
 	$("img").hide();
 	$("ul.menu").hide();
-	$(".sayHello").delay(23000).fadeOut();
-	$("img").delay(20000).show(1000);
-	$("ul.menu").delay(20000).show(1000);
+	$(".sayHello").delay(14000).fadeOut();
+	$("img").delay(12000).show(1000);
+	$("ul.menu").delay(12000).show(1000);
 }
 
 console.image("https://i.imgur.com/hKGenSr.jpg");
