de2b42cb7b01c4b338f2153fbb80c5865ce218b3
lovincyrus <hello@lovincyrus.com>
2017-04-08 15:15:53 -0700

    Add Tilt.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,60 @@
-// sayHello
-document.addEventListener("DOMContentLoaded", myFunction);
+// + 1 for each refresh
+if(sessionStorage){
+	// store
+    sessionStorage.setItem("is_loaded", 1);
 
-function myFunction() {
-	Typed.new(".sayHello", {
-	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
-	// loop: true
-	// typeSpeed: 30
-	});
+    // check if it's stored
+    if(sessionStorage.getItem("is_loaded") == 1) {
+    	// alert("true");
+    	//sessionStorage.getItem("is_loaded")
 
-	$("img").hide();
-	$("ul.menu").hide();
-	$(".sayHello").delay(12000).fadeOut();
-	$("img").delay(12500).show(1000);
-	$("ul.menu").delay(12500).show(1000);
+    	// sayHello
+		$(document).ready(function() {
+			Typed.new(".sayHello", {
+			strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
+			// loop: true
+			// typeSpeed: 30
+			});
+
+			$("img").hide();
+			$("ul.menu").hide();
+			$(".sayHello").delay(12000).fadeOut();
+			$("img").delay(12500).show(1000);
+			$("ul.menu").delay(12500).show(1000);
+		});
+
+		// click to skip
+		$(window).click(function() {
+			$(".sayHello").hide();
+			$("img").show();
+			$("ul.menu").show();
+		}).keydown(function() {
+			$(".sayHello").hide();
+			$("img").show();
+			$("ul.menu").show();
+		});
+	} else {
+		$(".sayHello").hide();
+	    alert("Walao, your browser do not support session storage.");
+	}
 }
+
+// sayHello
+// document.addEventListener("DOMContentLoaded", myFunction);
+
+// function myFunction() {
+// 	Typed.new(".sayHello", {
+// 	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
+// 	// loop: true
+// 	// typeSpeed: 30
+// 	});
+
+// 	$("img").hide();
+// 	$("ul.menu").hide();
+// 	$(".sayHello").delay(12000).fadeOut();
+// 	$("img").delay(12500).show(1000);
+// 	$("ul.menu").delay(12500).show(1000);
+// }
 
 // triggered click or keyboard to skip sayHello
 $(window).click(function() {
