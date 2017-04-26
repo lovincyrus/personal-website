c16252afd253d57bb4a5d4aa0a747bd2b7ca458c
lovincyrus <hello@lovincyrus.com>
2017-04-26 01:51:01 -0700

    Add sessionStorage to dismiss sayHello

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,35 @@
-// + 1 for each refresh
-if(sessionStorage){
-	// store
-    sessionStorage.setItem("is_loaded", 1);
+// Typed.js
+function sayHello() {
+	Typed.new(".sayHello", {
+	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
+	// loop: true,
+	// typeSpeed: 30
+	});
 
-    // check if it's stored
-    if(sessionStorage.getItem("is_loaded") == 1) {
-    	// alert("true");
-    	//sessionStorage.getItem("is_loaded")
+	$("img").hide();
+	$("ul.menu").hide();
+	$(".sayHello").delay(12000).fadeOut();
+	$("img").delay(12500).show(1000);
+	$("ul.menu").delay(12500).show(1000);
 
-    	// sayHello
-		$(document).ready(function() {
-			Typed.new(".sayHello", {
-			strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
-			// loop: true
-			// typeSpeed: 30
-			});
-
-			$("img").hide();
-			$("ul.menu").hide();
-			$(".sayHello").delay(12000).fadeOut();
-			$("img").delay(12500).show(1000);
-			$("ul.menu").delay(12500).show(1000);
-		});
-
-		// click to skip
-		$(window).click(function() {
-			$(".sayHello").hide();
-			$("img").show();
-			$("ul.menu").show();
-		}).keydown(function() {
-			$(".sayHello").hide();
-			$("img").show();
-			$("ul.menu").show();
-		});
-	} else {
+	$(document).click(function() {
 		$(".sayHello").hide();
-	    // alert("Walao, your browser do not support session storage.");
-	}
+		$("img").show();
+		$("ul.menu").show();
+	}).keydown(function() {
+		$(".sayHello").hide();
+		$("img").show();
+		$("ul.menu").show();
+	});
 }
 
-// sayHello
-// document.addEventListener("DOMContentLoaded", myFunction);
-
-// function myFunction() {
-// 	Typed.new(".sayHello", {
-// 	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
-// 	// loop: true
-// 	// typeSpeed: 30
-// 	});
-
-// 	$("img").hide();
-// 	$("ul.menu").hide();
-// 	$(".sayHello").delay(12000).fadeOut();
-// 	$("img").delay(12500).show(1000);
-// 	$("ul.menu").delay(12500).show(1000);
-// }
-
-// triggered click or keyboard to skip sayHello
-$(window).click(function() {
-	$(".sayHello").hide();
-	$("img").show();
-	$("ul.menu").show();
-}).keydown(function() {
-	$(".sayHello").hide();
-	$("img").show();
-	$("ul.menu").show();
-});
+if (!sessionStorage.getItem("is_loaded")) {
+	sayHello();
+	sessionStorage.setItem("is_loaded", true);
+} else if (sessionStorage.getItem("is_loaded") == true) {
+	console.log("it's loaded bruh");
+} else { console.log("oh well"); }
+	
 
 // meme
 console.image("https://i.imgur.com/hKGenSr.jpg");
