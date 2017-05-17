6670c273daf41395c411d572b035af4122d1c2b5
lovincyrus <hello@lovincyrus.com>
2017-05-17 01:34:42 -0700

    70% done with new about layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // Typed.js
 function sayHello() {
-	Typed.new(".sayHello", {
+	Typed.new("#sayHello", {
 	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
 	// loop: true,
 	// typeSpeed: 30
@@ -8,18 +8,30 @@ function sayHello() {
 
 	$("img").hide();
 	$("ul.menu").hide();
-	$(".sayHello").delay(12000).fadeOut();
+	$("h1.hero").hide();
+	$("p.description").hide();
+	$("code").hide();
+	$("#sayHello").delay(12000).fadeOut();
 	$("img").delay(12500).fadeIn(1000);
 	$("ul.menu").delay(12500).fadeIn(1000);
+	$("h1.hero").delay(12500).fadeIn(1000);
+	$("p.description").delay(12500).fadeIn(1000);
+	$("code").delay(12500).fadeIn(1000);
 
 	$(document).click(function() {
-		$(".sayHello").hide();
+		$("#sayHello").hide();
 		$("img").fadeIn();
 		$("ul.menu").fadeIn();
+		$("h1.hero").fadeIn();
+		$("p.description").fadeIn();
+		$("code").fadeIn();
 	}).keydown(function() {
-		$(".sayHello").hide();
+		$("#sayHello").hide();
 		$("img").fadeIn();
 		$("ul.menu").fadeIn();
+		$("h1.hero").fadeIn();
+		$("p.description").fadeIn();
+		$("code").fadeIn();
 	});
 }
 
@@ -35,7 +47,11 @@ if (!sessionStorage.getItem("is_loaded")) {
 console.image("https://i.imgur.com/hKGenSr.jpg");
 
 
-$(".menu a").on("click", function(){
-   $(".menu").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+$(function() {
+   $(".menu").click(function() {
+      // remove classes from all
+      $(".menu").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
 });
