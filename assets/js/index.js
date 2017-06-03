ea5a3268fc053e9b64a8e05467ed083ff1204036
lovincyrus <hello@lovincyrus.com>
2017-06-02 21:15:10 -0700

    Add router vuejs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,83 @@
-/**
+// function sayHello() {
+// 	Typed.new("#sayHello", {
+// 	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
+// 	// loop: true,
+// 	// typeSpeed: 30
+// 	});
 
-... and you want click on the thumbnail to open PhotoSwipe with large image (like it's done on a demo page). All you need to do is:
+// 	// hide these lads
+// 	$("img").hide();
+// 	$("ul.menu-container").hide();
+// 	$("h1.hero").hide();
+// 	$("p.description").hide();
+// 	$("code").hide();
+// 	$("li.hook-me-up").hide();
+// 	$("a.social-media").hide();
+// 	$("#projects-container").hide();
 
-1. Bind click event to links/thumbnails.
-2. After user clicked on on thumbnail, find its index.
-3. Create an array of slide objects from DOM elements – loop through all links and retrieve href attribute (large image url), data-size attribute (its size), src of thumbnail, and contents of caption.
+// 	// fadeout
+// 	$("#sayHello").delay(12000).fadeOut();
+// 	$("img").delay(12500).fadeIn(1000);
 
-**/
+// 	// fadein
+// 	$("ul.menu-container").delay(12500).fadeIn(1000);
+// 	$("h1.hero").delay(12500).fadeIn(1000);
+// 	$("p.description").delay(12500).fadeIn(1000);
+// 	$("code").delay(12500).fadeIn(1000);
+// 	$("li.hook-me-up").delay(12500).fadeIn(1000);
+// 	$("a.social-media").delay(12500).fadeIn(1000);
 
-// then build Responsive Images by adding mediumImage & originalImage
+// 	$(document).click(function() {
+// 		$("#sayHello").hide();
+// 		$("img").fadeIn();
+// 		$("ul.menu-container").fadeIn();
+// 		$("h1.hero").fadeIn();
+// 		$("p.description").fadeIn();
+// 		$("code").fadeIn();
+// 		$("li.hook-me-up").fadeIn();
+// 		$("a.social-media").fadeIn();
+// 	}).keydown(function() {
+// 		$("#sayHello").hide();
+// 		$("img").fadeIn();
+// 		$("ul.menu-container").fadeIn();
+// 		$("h1.hero").fadeIn();
+// 		$("p.description").fadeIn();
+// 		$("code").fadeIn();
+// 		$("li.hook-me-up").fadeIn();
+// 		$("a.social-media").fadeIn();
+// 	});
+// }
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
+// if (!sessionStorage.getItem("is_loaded")) {
+// 	sayHello();
+// 	sessionStorage.setItem("is_loaded", true);
+// }
 
-// build items array
-var items = [
-    {
-        src: 'assets/img/photos/1.png',
-        w: 1200,
-        h: 900
-    },
-    {
-        src: 'assets/img/photos/2.png',
-        w: 1200,
-        h: 900
-    },
-    {
-        src: 'assets/img/photos/3.png',
-        w: 1200,
-        h: 900
-    },
-    {
-        src: 'assets/img/photos/4.jpg',
-        w: 520,
-        h: 720
-    }
+// console.image("https://i.imgur.com/hKGenSr.jpg");
 
-];
+// change nav
+$(document).ready(function () {
+    // $('a[href^="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $('a').each(function () {
+    //         $(this).removeClass('active');
+    //     })
+    //     $(this).addClass('active');
+    // });
+});
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    // showAnimationDuration: 0,
-    // hideAnimationDuration: 0
-    index: 0 // start at first slide
-};
+// switch nav content
+$(document).ready(function(){
+	// document.getElementById("projects-container").style.display = 'none';
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-// gallery.init();
+ //    $("a#about").click(function(){
+	// 	document.getElementById("main-container").style.display = '';
+	// 	document.getElementById("contact-container").style.display = '';
+	// 	document.getElementById("projects-container").style.display = 'none';
+ //    });
+	// $("/projects").click(function(){
+	// 	document.getElementById("main-container").style.display = 'none';
+	// 	document.getElementById("contact-container").style.display = 'none';
+	// 	document.getElementById("projects-container").style.display = '';
+	// });
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,154 @@
-function sayHello() {
-	Typed.new("#sayHello", {
-	strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
-	// loop: true,
-	// typeSpeed: 30
-	});
+// Define route components.
+const About = { template: `<div id="main">
+	<div id="main-container">
+		<div class="about">
+			<h1 class="hero">Cyrus Goh</h1>
+			<p class="description">
+				I am a full stack web developer, front end engineer, an avid Python enthusiast and a cognitive science student at UC Davis. I love to learn by doing things, which is why I am <em>always</em> in progress. I firmly believe that it is not what technology stack you use that make the product great, it is how you implement it. While technology frameworks may not last, but photographs definitely will — you may check out my  <a href="https://vsco.co/lovincyrus" target="blank_" class="here"><strong>photography work</strong></a>.<br><br>My utmost passion is to connect <em>human</em> and <em>technology</em>, and to build <em>user-centric</em> products.</p>
+			<code>[!] Currently accepting new projects for Summer 2017 or remote.</code>
+		</div>
+	</div>
 
-	// hide these lads
-	$("img").hide();
-	$("ul.menu-container").hide();
-	$("h1.hero").hide();
-	$("p.description").hide();
-	$("code").hide();
-	$("li.hook-me-up").hide();
-	$("a.social-media").hide();
-	$("#projects-container").hide();
+	<div id="contact-container">
+		<div class="contact">
+			<ul class="link">
+				<li class="hook-me-up">twitter</li>
+				<a href="https://twitter.com/cyrsgh" class="social-media" target="_blank">/cyrsgh</a>
 
-	// fadeout
-	$("#sayHello").delay(12000).fadeOut();
-	$("img").delay(12500).fadeIn(1000);
+				<li class="hook-me-up">github</li>
+				<a href="https://github.com/lovincyrus" class="social-media" target="_blank">/lovincyrus</a>
 
-	// fadein
-	$("ul.menu-container").delay(12500).fadeIn(1000);
-	$("h1.hero").delay(12500).fadeIn(1000);
-	$("p.description").delay(12500).fadeIn(1000);
-	$("code").delay(12500).fadeIn(1000);
-	$("li.hook-me-up").delay(12500).fadeIn(1000);
-	$("a.social-media").delay(12500).fadeIn(1000);
+				<li class="hook-me-up">linkedin</li>
+				<a href="https://www.linkedin.com/in/cyrusgoh" class="social-media" target="_blank">/in/cyrusgoh</a>
 
-	$(document).click(function() {
-		$("#sayHello").hide();
-		$("img").fadeIn();
-		$("ul.menu-container").fadeIn();
-		$("h1.hero").fadeIn();
-		$("p.description").fadeIn();
-		$("code").fadeIn();
-		$("li.hook-me-up").fadeIn();
-		$("a.social-media").fadeIn();
-	}).keydown(function() {
-		$("#sayHello").hide();
-		$("img").fadeIn();
-		$("ul.menu-container").fadeIn();
-		$("h1.hero").fadeIn();
-		$("p.description").fadeIn();
-		$("code").fadeIn();
-		$("li.hook-me-up").fadeIn();
-		$("a.social-media").fadeIn();
-	});
-}
+				<li class="hook-me-up">email</li>
+				<a href="mailto:hello@lovincyrus.com" class="social-media">hello@lovincyrus.com</a>
+			</ul>
+		</div>
+	</div>
+</div>` }
 
-if (!sessionStorage.getItem("is_loaded")) {
-	sayHello();
-	sessionStorage.setItem("is_loaded", true);
-}
+const Projects = { template: `	<div id="projects-container">
+		<a class="link" href="https://github.com/lovincyrus/joyfeed" target="_blank">
+			<div class="container joyfeed-background mouseover">
+				<h1 class="pro-name">joyfeed</h1>
+				<p class="brief">Analyzes human emotions using machine learning and filters negative posts.</p>
+			</div>
+		</a>
 
-console.image("https://i.imgur.com/hKGenSr.jpg");
+		<a class="link" href="https://www.lovincyrus.com/fhda" target="_blank">
+			<div class="container fhda-background mouseover">
+				<h1 class="pro-name">fhda</h1>
+				<p class="brief">Gathers FHDA International Alumni.</p>
+			</div>
+		</a>
 
-// change nav
-$(document).ready(function () {
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-    });
-});
+		<a class="link" href="https://github.com/lovincyrus/gimo" target="_blank">
+			<div class="container gimo-background mouseover">
+				<h1 class="pro-name">gimo</h1>
+				<p class="brief">Activity tracker but for GitHub streak.</p>
+			</div>
+		</a>
 
-// switch nav content
-$(document).ready(function(){
-	document.getElementById("projects-container").style.display = 'none';
+		<a class="link" href="https://github.com/lovincyrus/penguiin" target="_blank">
+			<div class="container penguiin-background mouseover">
+				<h1 class="pro-name">penguiin</h1>
+				<p class="brief">NASA Space Apps Challenge 2017</p>
+			</div>
+		</a>
 
-    $("a#about").click(function(){
-		document.getElementById("main-container").style.display = '';
-		document.getElementById("right-main").style.display = '';
-		document.getElementById("projects-container").style.display = 'none';
-    });
-	$("a#projects").click(function(){
-		document.getElementById("main-container").style.display = 'none';
-		document.getElementById("right-main").style.display = 'none';
-		document.getElementById("projects-container").style.display = '';
-	});
-});
+		<a class="link" href="https://github.com/lovincyrus/terminal-quotes" target="_blank">
+			<div class="container quotes-background mouseover">
+				<h1 class="pro-name">terminal quotes</h1>
+				<p class="brief">Display quotes on Terminal.</p>
+			</div>
+		</a>
+	</div>` }
+
+
+
+
+// Define some routes
+const routes = [
+	{ path: '/about', component: About, alias: '/' },
+	{ path: '/projects', component: Projects }
+]
+
+
+// Create the router instance and pass the `routes` option
+const router = new VueRouter({
+	routes
+})
+
+
+// Create and mount the root instance.
+const app = new Vue({
+	router,
+	mounted: function() {
+		if (!sessionStorage.getItem("is_loaded")) {
+			this.sayHello();
+			sessionStorage.setItem("is_loaded", true);
+		}
+
+		$('a[href^="#"]').on('click', function (e) {
+	        e.preventDefault();
+	        $('a').each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
+	    });
+
+	    console.image("https://i.imgur.com/hKGenSr.jpg");
+	},
+	methods: {
+		sayHello: function() {
+			Typed.new("#sayHello", {
+				strings: ["Hello. ^100", "I'm glad that you're here. ^100", "No, no, no! ^10", "I'm really happy, pleased, contented, delighted, thrilled, overjoyed, elated, gleeful and thankful that you're here. ^100"]
+				// loop: true,
+				// typeSpeed: 30
+				});
+
+				// hide these lads
+				$("img").hide();
+				$("ul.menu-container").hide();
+				$("h1.hero").hide();
+				$("p.description").hide();
+				$("code").hide();
+				$("li.hook-me-up").hide();
+				$("a.social-media").hide();
+				$("#projects-container").hide();
+
+				// fadeout
+				$("#sayHello").delay(12000).fadeOut();
+				$("img").delay(12500).fadeIn(1000);
+
+				// fadein
+				$("ul.menu-container").delay(12500).fadeIn(1000);
+				$("h1.hero").delay(12500).fadeIn(1000);
+				$("p.description").delay(12500).fadeIn(1000);
+				$("code").delay(12500).fadeIn(1000);
+				$("li.hook-me-up").delay(12500).fadeIn(1000);
+				$("a.social-media").delay(12500).fadeIn(1000);
+
+				$(document).click(function() {
+					$("#sayHello").hide();
+					$("img").fadeIn();
+					$("ul.menu-container").fadeIn();
+					$("h1.hero").fadeIn();
+					$("p.description").fadeIn();
+					$("code").fadeIn();
+					$("li.hook-me-up").fadeIn();
+					$("a.social-media").fadeIn();
+				}).keydown(function() {
+					$("#sayHello").hide();
+					$("img").fadeIn();
+					$("ul.menu-container").fadeIn();
+					$("h1.hero").fadeIn();
+					$("p.description").fadeIn();
+					$("code").fadeIn();
+					$("li.hook-me-up").fadeIn();
+					$("a.social-media").fadeIn();
+				});
+		}
+	}
+}).$mount('#navbar')
