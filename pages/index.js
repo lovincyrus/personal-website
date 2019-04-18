cd7179c9ab76a362bc8b011bb206a956d1539f53
Cyrus Goh <hello@lovincyrus.com>
2019-04-18 12:10:03 -0700

    Add basic metatags

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,33 @@ export default () => (
         content="initial-scale=1.0, width=device-width"
         key="viewport"
       />
+      <meta
+        name="description"
+        content="Cyrus Goh @cyrsgh · Software Engineer · Thinker"
+      />
+      <meta name="author" content="Cyrus Goh" />
+      <meta property="og:site_name" content="Cyrus Goh" />
+      <meta property="og:image" content="URL" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Cyrus Goh" />
+      <meta
+        property="og:description"
+        content="Cyrus Goh @cyrsgh · Frontend Engineer · Thinker"
+      />
+      <meta property="og:url" content="https://www.lovincyrus.com" />
+      <meta
+        name="keywords"
+        content="cyrus goh, lovincyrus, css, css3, html, html5, JavaScript, Python, React.js, Web Development, frontend, front-end, fullstack, web design, UI, user interface, UX, user experience, Software Engineering, Linux, Git, Flask, Algorithms & Data Structures, Infrastructure, Front-End Development, Github, portfolio, projects, firebase, leadership, multi-tasking, teamwork, product thinking, empathetic, detail-oriented, results-oriented, efficiency, creative, analytical, aggiehackers, Aggie Hackers, California, Silicon Valley, Davis, Mountain View, Kuala Lumpur, San Francisco, Malaysia"
+      />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content="URL" />
+      <meta name="twitter:site" content="@cyrsgh" />
+      <meta name="twitter:title" content="Cyrus Goh" />
+      <meta
+        name="twitter:description"
+        content="Cyrus Goh @cyrsgh · Frontend Engineer · Thinker"
+      />
     </Head>
 
     <Header />
